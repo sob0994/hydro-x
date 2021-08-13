d67681cf61d47ebbcf12e2aad7481b7ee608c190
sob0994 <sob0994@gmail.com>
2021-08-13 12:21:43 +0800

    add header

--- src/pages/hydro/Dashboard.jsx
+++ src/pages/hydro/Dashboard.jsx
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
 import {
+  Button,
   Grid,
   Header,
+  Image,
   Segment,
   Statistic,
+  Table,
   Transition,
 } from "semantic-ui-react";
 import dateformat from "dateformat";
+import img1 from "../../assets/img.svg";
+import img2 from "../../assets/logo.png";
 
 const Dashboard = () => {
+  const [data, setData] = useState([
+    {
+      waktu: dateformat(Date.now(), "dd-mm-yyyy HH:MM:ss"),
+      tds: 200.0,
+      flow: 200.0,
+    },
+    {
+      waktu: dateformat(Date.now(), "dd-mm-yyyy HH:MM:ss"),
+      tds: 300.0,
+      flow: 400.0,
+    },
+  ]);
   const [jam, setJam] = useState(dateformat(Date.now(), "HH:MM:ss"));
   const [load, setLoad] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -26,27 +48,57 @@ const Dashboard = () => {
 
   return (
     <Grid padded stackable>
-      <Transition visible={!load} animation="drop" duration={1000}>
-        <Grid.Row columns="1">
-          <Grid.Column style={{ paddingTop: "3rem" }}>
-            <Header as="h1" textAlign="center" color="grey">
+      <Grid.Row columns="2">
+        <Grid.Column style={{ display: "flex", alignItems: "center" }}>
+          <Transition visible={!load} animation="drop" duration={1000}>
+            <Image
+              src={img2}
+              height="100px"
+              width="100px"
+              style={{ padding: "1rem" }}
+            />
+          </Transition>
+          <Transition visible={!load} animation="drop" duration={1000}>
+            <Header style={{ margin: 0 }}>
               <Header.Content>
-                HYDRO-X
+                SISTEM KONTROL OTOMATIS KEPEKATAN AIR NUTRISI HIDROPONIK
+                BERBASIS INTERNET OF THINGS (IOT)
+                <Header.Subheader>
+                  I WAYAN SUSTRISNA PUTRA (1715344007)
+                </Header.Subheader>
+                <Header.Subheader>
+                  PROGRAM STUDI D4 TEKNIK OTOMASI
+                </Header.Subheader>
+                <Header.Subheader>JURUSAN TEKNIK ELEKTRO</Header.Subheader>
+              </Header.Content>
+            </Header>
+          </Transition>
+        </Grid.Column>
+        <Grid.Column style={{ display: "flex", alignItems: "center" }}>
+          <Transition visible={!load} animation="drop" duration={1000}>
+            <Header
+              style={{ margin: "auto" }}
+              as="h1"
+              textAlign="center"
+              color="grey"
+            >
+              <Header.Content>
+                SMART HYDROPONIC
                 <Header.Subheader>
                   New Technology of Hydroponic
                 </Header.Subheader>
               </Header.Content>
             </Header>
-          </Grid.Column>
-        </Grid.Row>
-      </Transition>
+          </Transition>
+        </Grid.Column>
+      </Grid.Row>
       <Grid.Row columns="3">
         <Transition visible={!load} animation="slide down" duration={1200}>
           <Grid.Column>
             <Segment
               textAlign="center"
               color="blue"
-              style={{ overflow: "auto" }}
+              style={{ overflow: "hidden" }}
             >
               <Statistic label="Today" value={jam} color="blue" />
             </Segment>
@@ -55,39 +107,100 @@ const Dashboard = () => {
         <Transition visible={!load} animation="slide down" duration={1200}>
           <Grid.Column>
             <Segment textAlign="center" color="violet">
-              <Statistic label="PPM" value={0} color="violet" />
+              <Statistic color="violet">
+                <Statistic.Value>0.00 PPM</Statistic.Value>
+                <Statistic.Label>TDS</Statistic.Label>
+              </Statistic>
             </Segment>
           </Grid.Column>
         </Transition>
         <Transition visible={!load} animation="slide down" duration={1200}>
           <Grid.Column>
             <Segment textAlign="center" color="purple">
-              <Statistic label="Flow Rate" value={0} color="purple" />
+              {/* <Statistic label="Flow Rate" value={0} color="purple" /> */}
+              <Statistic color="purple">
+                <Statistic.Value>0.00 MM</Statistic.Value>
+                <Statistic.Label>FLOW METER</Statistic.Label>
+              </Statistic>
             </Segment>
           </Grid.Column>
         </Transition>
       </Grid.Row>
-      <Transition visible={!load} animation="slide up" duration={1200}>
-        <Grid.Row columns="1">
+      <Grid.Row centered columns="1">
+        {/* <div> */}
+        <Button
+          color="violet"
+          onClick={handleOpen}
+          style={{ marginBottom: "1rem" }}
+        >
+          {isOpen ? "Hide History Data" : "Show History Data"}
+        </Button>
+        {/* </div> */}
+
+        <Transition visible={isOpen} animation="slide down" duration={1200}>
           <Grid.Column>
-            <Segment textAlign="center" color="orange">
-              No History Data
+            <Segment
+              textAlign="center"
+              color="orange"
+              style={{ overflow: "auto" }}
+            >
+              <Table size="small" celled unstackable striped fluid>
+                <Table.Header>
+                  <Table.Row textAlign="center">
+                    <Table.HeaderCell>Day</Table.HeaderCell>
+                    <Table.HeaderCell>TDS</Table.HeaderCell>
+                    <Table.HeaderCell>Flow Meter</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {data.map((data, idx) => {
+                    return (
+                      <Table.Row keys={idx} textAlign="center">
+                        <Table.Cell>{data.waktu}</Table.Cell>
+                        <Table.Cell>{data.tds}</Table.Cell>
+                        <Table.Cell>{data.flow}</Table.Cell>
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+              </Table>
             </Segment>
           </Grid.Column>
-        </Grid.Row>
-      </Transition>
+        </Transition>
+      </Grid.Row>
       <Transition visible={!load} animation="slide down" duration={1500}>
         <Grid.Row columns="1">
           <Grid.Column>
             <Header as="h1" textAlign="center" color="grey">
               <Header.Content>
                 <Header.Subheader style={{ fontSize: "0.9rem" }}>
-                  Copyright Ⓒ 2021
+                  <b>Dibiayai Oleh :</b>
+                  <br />
+                  Penelitian Unggulan Strategis P3M PNB
+                  <br />
+                  Dr. Anak Agung Ngurah Gde Sapteka, CIRR.
                 </Header.Subheader>
               </Header.Content>
             </Header>
           </Grid.Column>
         </Grid.Row>
+      </Transition>
+
+      <Transition visible={!load} animation="slide down" duration={1500}>
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            bottom: 0,
+            left: 0,
+            background: `url(${img1})`,
+            backgroundPosition: "left bottom",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "40vh 40vh",
+            zIndex: -1,
+          }}
+        ></div>
       </Transition>
     </Grid>
   );
