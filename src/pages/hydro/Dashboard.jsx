31ca98befeb67ea140bd3fd9ea24eee16d8f3020
sob0994 <sob0994@gmail.com>
2021-08-13 09:34:22 +0800

    animation

--- src/pages/hydro/Dashboard.jsx
+++ src/pages/hydro/Dashboard.jsx
@@ -1,63 +1,94 @@
-import React, { useEffect, useState } from 'react'
-import { Grid, Header, Segment, Statistic } from 'semantic-ui-react'
-import dateformat from 'dateformat'
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Header,
+  Segment,
+  Statistic,
+  Transition,
+} from "semantic-ui-react";
+import dateformat from "dateformat";
 
 const Dashboard = () => {
-    const [jam, setJam]=useState(dateformat(Date.now(),"HH:MM:ss"))
+  const [jam, setJam] = useState(dateformat(Date.now(), "HH:MM:ss"));
+  const [load, setLoad] = useState(true);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setJam(dateformat(Date.now(),"HH:MM:ss"))
-        },1000)
-    })
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 500);
+  }, []);
 
-    return (
-        <Grid padded stackable>
-            <Grid.Row columns='1'>
-                <Grid.Column style={{paddingTop:'3rem'}}>
-                    <Header as='h1' textAlign='center' color='grey'>
-                        <Header.Content>
-                            HYDRO-X
-                        <Header.Subheader>New Technology of Hydroponic</Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns='3'>
-                <Grid.Column>
-                    <Segment textAlign='center' color='blue' style={{overflow:'auto'}}>
-                        <Statistic label='Today' value={jam} color='blue' />
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Segment textAlign='center' color='violet'>
-                        <Statistic label='PPM' value={0} color='violet' />
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column>
-                    <Segment textAlign='center' color='purple'>
-                        <Statistic label='Flow Rate' value={0} color='purple' />
-                    </Segment>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns='1'>
-                <Grid.Column>
-                    <Segment textAlign='center' color='orange'>
-                        No History Data
-                    </Segment>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns='1'>
-                <Grid.Column>
-                    <Header as='h1' textAlign='center' color='grey'>
-                        <Header.Content>
-                        <Header.Subheader style={{fontSize:'0.9rem'}}>Copyright Ⓒ 2021</Header.Subheader>
-                        </Header.Content>
-                    </Header>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    )
-}
+  useEffect(() => {
+    setTimeout(() => {
+      setJam(dateformat(Date.now(), "HH:MM:ss"));
+    }, 1000);
+  });
 
-export default Dashboard
+  return (
+    <Grid padded stackable>
+      <Transition visible={!load} animation="drop" duration={1000}>
+        <Grid.Row columns="1">
+          <Grid.Column style={{ paddingTop: "3rem" }}>
+            <Header as="h1" textAlign="center" color="grey">
+              <Header.Content>
+                HYDRO-X
+                <Header.Subheader>
+                  New Technology of Hydroponic
+                </Header.Subheader>
+              </Header.Content>
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Transition>
+      <Transition visible={!load} animation="drop" duration={1400}>
+        <Grid.Row columns="3">
+          <Grid.Column>
+            <Segment
+              textAlign="center"
+              color="blue"
+              style={{ overflow: "auto" }}
+            >
+              <Statistic label="Today" value={jam} color="blue" />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center" color="violet">
+              <Statistic label="PPM" value={0} color="violet" />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center" color="purple">
+              <Statistic label="Flow Rate" value={0} color="purple" />
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Transition>
+
+      <Transition visible={!load} animation="drop" duration={1800}>
+        <Grid.Row columns="1">
+          <Grid.Column>
+            <Segment textAlign="center" color="orange">
+              No History Data
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Transition>
+
+      <Transition visible={!load} animation="drop" duration={2200}>
+        <Grid.Row columns="1">
+          <Grid.Column>
+            <Header as="h1" textAlign="center" color="grey">
+              <Header.Content>
+                <Header.Subheader style={{ fontSize: "0.9rem" }}>
+                  Copyright Ⓒ 2021
+                </Header.Subheader>
+              </Header.Content>
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Transition>
+    </Grid>
+  );
+};
+
+export default Dashboard;
